71260c1fe9d3c5568fcc7fa6a1ecae8999ef1ff8
sbn04043 <sbn04043@gmail.com>
2022-03-22 21:59:36 +0900

    친구추가 기능

--- src/pages/Friends/data.ts
+++ src/pages/Friends/data.ts
@@ -1,19 +1,18 @@
 type FriendType = {
-    id: number;
-    name: string;
-    statusMessage: string;
+  id: number;
+  name: string;
+  statusMessage: string;
 };
 
 export const friends: FriendType[] = [
-    {
-        id: 1111,
-        name: "친구1",
-        statusMessage: "상태메세지"
-    },
-    {
-        id: 2222,
-        name: "친구2",
-        statusMessage: "상태메세지"
-    }
+  {
+    id: 1111,
+    name: "친구1",
+    statusMessage: "상태메세지",
+  },
+  {
+    id: 2222,
+    name: "친구2",
+    statusMessage: "상태메세지",
+  },
 ];
-
--- src/pages/Friends/index.tsx
+++ src/pages/Friends/index.tsx
@@ -1,70 +1,90 @@
 import {
-    Avatar,
-    Container,
-    List,
-    ListItemAvatar,
-    ListItemButton,
-    ListItemText,
-    TextField,
+  Avatar,
+  Container,
+  IconButton,
+  List,
+  ListItemAvatar,
+  ListItemButton,
+  ListItemText,
+  Modal,
+  TextField,
 } from "@mui/material";
+
 import Box from "@mui/material/Box";
 import { friends } from "./data";
 import ImageIcon from "@mui/icons-material/Image";
-import { ChangeEvent, useState } from "react";
-import Grid from '@mui/material/Grid';
-import Button from '@mui/material/Button';
-import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import React, { ChangeEvent, useState } from "react";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import FriendAdd from "./components/FriendAdd";
 
 const Friends = (): JSX.Element => {
-    const [findFriend, setFindFriend] = useState(friends);
+  const [findFriend, setFindFriend] = useState(friends);
 
-    const searchFriend = (event: ChangeEvent<HTMLInputElement>) => {
-        const inputText = event.currentTarget.value;
-        if (inputText === "") {
-            setFindFriend(friends);
-        } else {
-        const searching = friends.filter((friend) => {
-            return friend.name.includes(event.currentTarget.value);
-            })
-        setFindFriend(searching);
-        }
-    };
+  const [open, setOpen] = React.useState(false);
+  const openModal = () => setOpen(true);
+  const closeModal = () => setOpen(false);
 
-    return (
-        <Container>
-            <Box>
-                <Grid container>
-                    <Grid item xs={11}>
-            <TextField 
-                fullWidth 
-                id="outlined-basic" 
-                label="친구 검색" 
-                variant="outlined" 
-                margin="dense" 
-                onChange={searchFriend} 
-                />
-                </Grid>
-                <Grid item>
-                    <Button><PersonAddIcon /></Button>
-                </Grid>
-                </Grid>
+  const searchFriend = (event: ChangeEvent<HTMLInputElement>) => {
+    const inputText = event.currentTarget.value;
+    if (inputText === "") {
+      setFindFriend(friends);
+    } else {
+      const searching = friends.filter((friend) => {
+        return friend.name.includes(event.currentTarget.value);
+      });
+      setFindFriend(searching);
+    }
+  };
+
+  return (
+    <Container>
+      <Modal open={open} onClose={closeModal}>
+        <Box>
+          <FriendAdd />
+        </Box>
+      </Modal>
+      <Box>
+        <Grid container>
+          <Grid item xs={10.5}>
+            <TextField
+              fullWidth
+              id="outlined-basic"
+              label="친구 검색"
+              variant="outlined"
+              margin="dense"
+              onChange={searchFriend}
+            />
+          </Grid>
+          <Grid item xs={1.5}>
+            <Box sx={{ p: "8px" }}>
+              <IconButton color="primary" size="large" onClick={openModal}>
+                <PersonAddIcon />
+              </IconButton>
             </Box>
-            <List>
-                {findFriend.map((friend) => {
-                    return (
-                        <ListItemButton key={friend.id}>
-                            <ListItemAvatar>
-                                <Avatar>
-                                    <ImageIcon />
-                                </Avatar>
-                            </ListItemAvatar>
-                            <ListItemText primary={friend.name} secondary={friend.statusMessage} />
-                        </ListItemButton>
-                    )
-                })}
-            </List>
-        </Container>
-    )
-}
+          </Grid>
+        </Grid>
+      </Box>
+      <List>
+        {findFriend.map((friend) => {
+          return (
+            <ListItemButton key={friend.id}>
+              <ListItemAvatar>
+                <Avatar>
+                  <ImageIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary={friend.name}
+                secondary={friend.statusMessage}
+              />
+            </ListItemButton>
+          );
+        })}
+      </List>
+    </Container>
+  );
+};
 
 export default Friends;
